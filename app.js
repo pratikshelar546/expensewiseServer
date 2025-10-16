b3e24862643e2b61a3413369fa5b4a0a9a19d436
PratikRealx <pratik.shelar@realx.in>
2025-10-16 11:58:17 +0530

    added app.listin in condingtion

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 import {app} from "./api/index.js";
+if(process.env.NODE_ENV !== "production"){
+    app.listen(5000, "0.0.0.0",() => {
+        console.log(`Server Started at ${5000}`)
+    })
+}
 
-app.listen(5000, "0.0.0.0",() => {
-   
-    console.log(`Server Started at ${5000}`)
-})
