14234113a96ec74f9713746eb8558e8fc96bca01
PratikRealx <pratik.shelar@realx.in>
2025-10-16 11:42:34 +0530

    fix mongodb persisteing issue

--- api/index.js
+++ api/index.js
@@ -1,23 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from "body-parser";
-import expensesAPI from "../app/routes/expenses/index.js"
-import userAPI from "../app/routes/user/index.js";
-import organizationAPI from '../app/routes/organization/index.js';
-import expenseFeildAPI from "../app/routes/ExpenseField/index.js";
-import requestAPI from "../app/routes/Request/index.js";
-import privateConfig from "../app/Config/routeConfig.js";
-import passport from "passport";
+import dotenv from "dotenv";
+import express from "express";
 import session from "express-session";
-import DBConnection from "../app/DBModel/DBConnection.js";
+import passport from "passport";
 import serverless from "serverless-http";
+import privateConfig from "../app/Config/routeConfig.js";
+import DBConnection from "../app/DBModel/DBConnection.js";
+import expenseFeildAPI from "../app/routes/ExpenseField/index.js";
+import expensesAPI from "../app/routes/expenses/index.js";
+import organizationAPI from '../app/routes/organization/index.js';
+import requestAPI from "../app/routes/Request/index.js";
+import userAPI from "../app/routes/user/index.js";
+import MongoStore from "connect-mongo";
+const app = express();
 
 dotenv.config();
 
-const app = express();
+await DBConnection(); // Make sure you cache the connection inside this function
+
 privateConfig(passport);
 app.use(express.json());
+
 const corsOptions = {
     origin: "*",
     credentials: true,
@@ -25,13 +28,23 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '6mb' }));
+
 app.use(session({
     secret: "newProject",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB,
+        ttl: 14 * 24 * 60 * 60, // 14 days
+    }),
+    cookie: {
+        secure: process.env.NODE_ENV === "production", // only send over HTTPS
+        maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+    }
 }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '6mb' }));
 app.use((req, res, next) => {
     console.log(`Received request with body size: ${JSON.stringify(req.body).length} bytes`);
     next();
@@ -47,7 +60,6 @@ app.use(async (req, res, next) => {
     try {
         console.log("connecting to DB");
 
-        await DBConnection(); // Make sure you cache the connection inside this function
         console.log("connected");
 
         next();
@@ -62,12 +74,11 @@ app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
-app.get('/', function (req, res) {
-    console.log("hellosssss");
+app.get("/", (req, res) => {
+    if (!req.session.views) req.session.views = 0;
+    req.session.views++;
+    res.send(`Number of views: ${req.session.views}`);
+  });
 
-    res.send("hello")
-    // res.json({ message: "hello" })
-});
-
-app.listen(8000, () => console.log('Server ready on port 8000.'));
 export default serverless(app);
+export { app };
--- app.js
+++ app.js
@@ -1,50 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
-import DBConnect from "./app/DBModel/DBConnection.js"
-import cors from "cors";
-import bodyParser from "body-parser";
-import expensesAPI from "./app/routes/expenses/index.js"
-import userAPI from "./app/routes/user/index.js";
-import organizationAPI from './app/routes/organization/index.js';
-import expenseFeildAPI from "./app/routes/ExpenseField/index.js";
-import requestAPI from "./app/routes/Request/index.js";
-import privateConfig from "./app/Config/routeConfig.js";
-import passport from "passport";
-import session from "express-session";
 
-dotenv.config();
-
-const app = express();
-privateConfig(passport);
-app.use(express.json());
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-app.use(session({ secret: "newProject" }));
-app.use(bodyParser.json());
-app.use("/expenses", expensesAPI);
-app.use("/user", userAPI);
-app.use("/organization", organizationAPI);
-app.use("/field", expenseFeildAPI)
-app.use("/request", requestAPI)
-
-// app.use("/", (req, res) => {
-//     res.redirect('https://expensewisee.vercel.app/');
-// })
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
-
+import {app} from "./api/index.js";
 
 app.listen(5000, "0.0.0.0",() => {
-    DBConnect().then(() => {
-        console.log("server connected");
-    }).catch((error) => {
-        console.error(error);
-    })
-    console.log(`Server Started at ${3000}`)
+   
+    console.log(`Server Started at ${5000}`)
 })
